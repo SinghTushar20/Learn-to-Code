42f5906bbebc270da974fc9a21c42cbfe68fa772
dheeraj120501 <dheerajclan@gmail.com>
2022-03-12 22:17:30 +0530

    minor updates

--- 08-Languages/Python/01-Project_Project_Learning/OpenCV/Detection Using Haar Cascade/Mini Tesla/car_eyes.py
+++ 08-Languages/Python/01-Project_Project_Learning/OpenCV/Detection Using Haar Cascade/Mini Tesla/car_eyes.py
@@ -5,20 +5,20 @@ import cv2
 car_classifier = cv2.CascadeClassifier('haarcascade_cars.xml')
 pedestrian_classifier = cv2.CascadeClassifier('haarcascade_people.xml')
 
-## Detection in Image
-# img = cv2.imread("car1.jpg")
-# img_grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Detection in Image
+img = cv2.imread("image3.jpg")
+img_grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-## Detecting cars from images
-# cars = car_classifier.detectMultiScale(img_grayscale)
+# Detecting cars from images
+cars = car_classifier.detectMultiScale(img_grayscale)
 
-## Creating rectangles around the detected objects
-# for (x, y, w, h) in cars:
-#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+# Creating rectangles around the detected objects
+for (x, y, w, h) in cars:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-## Displaying the result
-# cv2.imshow("CAR VIEW", img)
-# cv2.waitKey()
+# Displaying the result
+cv2.imshow("CAR VIEW", img)
+cv2.waitKey()
 
 
 # Detection in Video
